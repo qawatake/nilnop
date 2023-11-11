3c1a195d7a4f8d75897cee423e0cf879f7ddba22
qawatake <38106890+qawatake@users.noreply.github.com>
2023-11-11 11:37:27 +0900

    test

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,10 +1,49 @@
 package a
 
+import "errors"
+
+func g() {
+	err := doSomething()
+	if err != nil {
+		panic("panic")
+	}
+	Wrap(err) // want "xx"
+}
+
+func h() (err error) {
+	Wrap(err) // want "xx"
+	err = errors.New("hoge")
+	Wrap(err) // ok
+	return err
+}
+
+func i() {
+	Wrap(nil) // want "xx"
+}
+
 func f() {
-	var x error
-	Wrap(x) // want "xx"
-	var s S
-	s.Wrap(nil) // want "xx"
+	var s []int = nil
+	Wrap(s) // want "xx"
+	ss := s[:][:]
+	Wrap(ss) // want "xx"
+}
+
+func f3() (a []int) {
+	Wrap(a) // want "xx"
+	return
+}
+
+func f2(x any) {
+	switch x.(type) {
+	case nil:
+		Wrap(x) // want "xx"
+	case int:
+		Wrap(x) // ok
+	}
+}
+
+func doSomething() error {
+	return nil
 }
 
 func Wrap(x any) {}
